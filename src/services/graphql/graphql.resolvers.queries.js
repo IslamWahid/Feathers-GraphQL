a5691defdc4158fbdca9f9b1594d36f5d12baf47
IslamWahid <eng.islamwahid@gmail.com>
2019-03-23 22:28:13 +0200

    feat: add functionality to use limit and skip

--- src/services/graphql/graphql.resolvers.queries.js
+++ src/services/graphql/graphql.resolvers.queries.js
@@ -1,14 +1,25 @@
+const omit = require('lodash/omit');
+
 export default function(Query, Service, GetServiceName, FindServiceName) {
   Object.assign(Query, {
     [`${GetServiceName}`]: (root, args, context) => {
-      return Service.find(Object.assign({}, context, { query: args })).then(result => result[0]);
-    },
+      return Service.find(Object.assign({}, context, { query: args })).then(
+        result => result[0]
+      );
+    }
   });
   Object.assign(Query, {
     [`${FindServiceName}`]: (root, args, context) => {
-      return Service.find(Object.assign({}, context, { query: args })).then(result => {
+      return Service.find(
+        Object.assign({}, context, {
+          query: Object.assign({}, omit(args, ['limit', 'skip']), {
+            $limit: args.limit,
+            $skip: args.skip
+          })
+        })
+      ).then(result => {
         return { total: result.length, items: result };
       });
-    },
+    }
   });
 }
